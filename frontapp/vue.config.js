68151790f39b5bf284f1976f74f29aa4e95e7bc2
Archi <dnrdl6817@gmail.com>
2025-05-19 09:25:30 +0900

    actions

--- frontapp/vue.config.js
+++ frontapp/vue.config.js
@@ -5,7 +5,7 @@ const server = "http://localhost:3000";
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  outputDir: path.resolve("../backend/public"), //build 위치 변경
+  // outputDir: path.resolve("../backend/public"), //build 위치 변경
   // 개발용 임시 서버
   devServer: {
     // Vue.js 실행 시 적용 PORT 변경
